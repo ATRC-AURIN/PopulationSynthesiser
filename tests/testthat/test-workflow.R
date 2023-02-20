411a4aa54849f338d57f854a7424d3dc946e4b20
asiripanich <17020181+asiripanich@users.noreply.github.com>
2023-02-20 11:00:43 +1100

    test: add a unit test for workflow.

---
+++ tests/testthat/test-workflow.R
@@ -0,0 +1,12 @@
+test_that("run_workflow()", {
+  results <- run_workflow(here::here("atrc_workflow/test_parameters.yaml"))
+  checkmate::expect_list(
+    results,
+    len = 2,
+    names = "unique"
+  )
+  checkmate::expect_names(
+    names(results),
+    identical.to = c("fitted_problem", "synthetic_population")
+  )
+})
